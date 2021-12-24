4f3a520a262f5b926d74072f2f46e42ab571f78a
Ricardo Ribeiro <ricardoribeiro.j7@gmail.com>
2021-12-23 21:44:08 -0300

    Finalized user CRUD.

--- frontend/src/Components/Modality/Modality.js
+++ frontend/src/Components/Modality/Modality.js
@@ -8,85 +8,85 @@ import ImgsPilatesSuspenso from '../imgs/pilates-suspenso.jpg';
 
 export default function Modality(){
     return(
-        <section class="section">
-            <div class="container has-text-centered">
+        <section className="section">
+            <div className="container has-text-centered">
             <Title>Modalidades</Title>
-            <div class="columns is-centered">
-            <div class="column">
-                        <div class="card">
-                            <div class="card-image">
-                                <figure class="image is-2by1">
-                                    <img src={ImgsMatPilates} alt="Placeholder image"/>
+            <div className="columns is-centered">
+            <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src={ImgsMatPilates} alt="Placeholder"/>
                                 </figure>
                             </div>
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <p class="title is-4">Mat Pilates</p>
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content">
+                                        <p className="title is-4">Mat Pilates</p>
                                     </div>
                                 </div>
 
-                                <div class="content">
+                                <div className="content">
                                     É uma formação clássica, no qual é abordado todos os exercícios originais desenvolvidos pelo Joseph Pilates no solo. Esses exercícios são o resultado da construção de ganho de força, flexibilidade e equilíbrio muscular.
                                 </div>
                             </div>
                         </div>
                 </div>
-                <div class="column">
-                        <div class="card">
-                            <div class="card-image">
-                                <figure class="image is-2by1">
-                                    <img src={ImgsPilatesSuspenso} alt="Placeholder image"/>
+                <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src={ImgsPilatesSuspenso} alt="Placeholder"/>
                                 </figure>
                             </div>
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <p class="title is-4">Pilates suspenso</p>
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content">
+                                        <p className="title is-4">Pilates suspenso</p>
                                     </div>
                                 </div>
 
-                                <div class="content">
+                                <div className="content">
                                 É uma modalidade usando como base os exercícios originais do Mat Pilates, mas usando a fita de suspensão, no qual diminui a descarga do peso no solo. Essa é uma modalidade que gera muita instabilidade, por isso treina a força, o equilíbrio.
                                 </div>
                             </div>
                         </div>
                 </div>
-                <div class="column">
-                        <div class="card">
-                            <div class="card-image">
-                                <figure class="image is-2by1">
-                                    <img src={ImgPilatesAcrobatico} alt="Placeholder image"/>
+                <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src={ImgPilatesAcrobatico} alt="Placeholder"/>
                                 </figure>
                             </div>
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <p class="title is-4">Pilates Acrobático</p>
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content">
+                                        <p className="title is-4">Pilates Acrobático</p>
                                     </div>
                                 </div>
 
-                                <div class="content">
+                                <div className="content">
                                 É uma modalidade de exercícios realizada nos equipamentos originais desenvolvido por Joseph Pilates, usando como base a suspensão de forma a diminuir a área de apoio gerando assim, maior ganho de força aos alunos.
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="column">
-                        <div class="card">
-                            <div class="card-image">
-                                <figure class="image is-2by1">
-                                    <img src={ImgPilatesParaGestante} alt="Placeholder image"/>
+                    <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src={ImgPilatesParaGestante} alt="Placeholder"/>
                                 </figure>
                             </div>
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <p class="title is-4">Pilates na gestação</p>
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content">
+                                        <p className="title is-4">Pilates na gestação</p>
                                     </div>
                                 </div>
 
-                                <div class="content">
+                                <div className="content">
                                 Essa é uma formação que vai agregar conhecimento para quem já trabalha com Pilates e quer aumentar o seu público de forma personalizada com base e conhecimento específico sobre todas as alterações que vão acontecer durante a gestação e a pós-gestação
                                 </div>
                             </div>
